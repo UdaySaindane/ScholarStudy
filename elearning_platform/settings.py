93041bf92c7576260da17ae915ea417614d1c98f
Uday Saindane <udaysaindane3421@gmail.com>
2025-10-22 19:39:09 +0530

    uncommenting dotenv

--- elearning_platform/settings.py
+++ elearning_platform/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 # Load local .env file
-# load_dotenv(Path(__file__).resolve().parent.parent / '.env')
+load_dotenv(Path(__file__).resolve().parent.parent / '.env')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
